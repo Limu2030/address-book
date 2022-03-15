b803348074e684b6ad41e13ab54b2ef43c341556
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-15 18:49:42 +0300

    script: It will make contact details show when clicked

--- script.js
+++ script.js
@@ -14,19 +14,26 @@ function Contact(first, last) {
       if (inputtedFirstName.length != ""){
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
   
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+        $("ul#contacts").append("<li><span class=\"contact\">" + newContact.firstName + " " + newContact.lastName + "</span></li>");
     
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
       }
+
+      if ($(".contact") != 0) {
+        $(".contact").last().click(function() {
+          alert($(".contact") != 0)
+          alert("contact clicked")
+          $("#show-contact").show(); // show-contact
+          $("#show-contact h2").text(newContact.firstName);
+          $(".first-name").text(newContact.firstName);
+          $(".last-name").text(newContact.lastName);
+        });  
+      }
     });
   }); 
-  $(".contact").click(function() {
-    $("#show-contact").show(); // show-contact
-    $("#show-contact h2").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-  });  
+  
+  
   // business logic
 function Contact(first, last) {
     this.firstName = first;
