0f106d32e5eaeceb4bc376f310b1f8d816eaff54
limu2030 <limu2030@gmail.com>
2022-03-16 12:09:02 +0300

    patch: adds two other fields

--- js/script.js
+++ js/script.js
@@ -1,45 +1,76 @@
 // business logic
-function Contact(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-  }
-  
-  // user interface logic
-  $(document).ready(function() {
-    $("form#new-contact").submit(function(event) {
-      event.preventDefault();
-  
-      var inputtedFirstName = $("input#new-first-name").val();
-      var inputtedLastName = $("input#new-last-name").val();
-      if (inputtedFirstName.length != ""){
-        var newContact = new Contact(inputtedFirstName, inputtedLastName);
-  
-        $("ul#contacts").append("<li><span class=\"contact\">" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-    
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
-      }
+function Contact(first, last, city, county) {
+  this.firstName = first;
+  this.lastName = last;
+  this.cityName = city;
+  this.countyName = county;
+}
 
-      if ($(".contact") != 0) {
-        $(".contact").last().click(function() {
-          alert($(".contact") != 0)
-          alert("contact clicked")
-          $("#show-contact").show(); // show-contact
+// user interface logic
+$(document).ready(function () {
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedCityName = $("input#new-city-name").val();
+    var inputtedCountyName = $("input#new-county-name").val();
+
+    if (inputtedFirstName.length != "") {
+      var newContact = new Contact(
+        inputtedFirstName,
+        inputtedLastName,
+        inputtedCityName,
+        inputtedCountyName
+      );
+
+      $("ul#contacts").append(
+        '<li><span class="contact">' +
+          newContact.firstName +
+          " " +
+          newContact.lastName,
+        "</span></li>"
+      );
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-city-name").val("");
+      $("input#new-county-name").val("");
+    }
+
+    if ($(".contact") != 0) {
+      $(".contact")
+        .last()
+        .click(function () {
+          alert($(".contact") != 0);
+          alert("contact clicked");
+          $("#show-contact").show(); 
           $("#show-contact h2").text(newContact.firstName);
           $(".first-name").text(newContact.firstName);
           $(".last-name").text(newContact.lastName);
-        });  
-      }
-    });
-  }); 
-  
-  
-  // business logic
-function Contact(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-  }
-  
-  Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
-  }
+          $(".city-name").text(newContact.cityName);
+          $(".county-name").text(newContact.countyName);
+        });
+    }
+  });
+});
+
+// business logic
+function Contact(first, last, city, county) {
+  this.firstName = first;
+  this.lastName = last;
+  this.cityName = city;
+  this.countyName = county;
+}
+
+Contact.prototype.fullName = function () {
+  return (
+    this.firstName +
+    " " +
+    this.lastName +
+    " " +
+    this.cityName +
+    " " +
+    this.countyName
+  );
+};
